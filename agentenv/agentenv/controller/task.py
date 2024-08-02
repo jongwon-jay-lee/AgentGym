fix: input_length handling by adding equal sign

agentenv/agentenv/controller/task.py
@@ -129,7 +129,7 @@ class BaseTask:
         while not done:
             input_length = len(conversation_tokenized["input_ids"])
             # if input_length exceeds 4096, break
-            if input_length > 4096:
+            if input_length >= 4096:
                 break
             output = model.generate(
                 torch.tensor(